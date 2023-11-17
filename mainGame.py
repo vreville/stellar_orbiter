6fb59967ef7f978f629226d6097baad74ef4dcfb
Victor Reville <victor.reville@irap.omp.eu>
2023-11-17 14:32:03 +0100

    remove trailing whitespace

--- mainGame.py
+++ mainGame.py
@@ -63,8 +63,8 @@ def cls():
 
 
 # Animations:
-# This function gets the first frame you want to show up. Then it gets the 
-# second frame you want to appear. Next, it gets the time between each frame. 
+# This function gets the first frame you want to show up. Then it gets the
+# second frame you want to appear. Next, it gets the time between each frame.
 # And lastly, the 'loops' (how many times you want the animation to repeat).
 def animation(frame1, frame2, timeBetweenFrames, loops):
     for i in range(loops):
@@ -91,14 +91,14 @@ def textAnimation(sentence, timeBetweenLetters=0.02):
         sleep(timeBetweenLetters)
     print("")
  
-# Make background go black (only for web Python interpreters):  
+# Make background go black (only for web Python interpreters):
 def blackground(sentence = ""):
     print('\x1b[40m''\x1b[97m')
     print(" " * 9999)
     cls()
 
 
-# Make background go white (only for web Python interpreters):     
+# Make background go white (only for web Python interpreters):
 def whiteBackground(sentence = ""):
     print('\u001b[0m')
     print(" " * 9999)
@@ -192,7 +192,7 @@ print(riddle3)
 mainQuest = menu("Sit back, relax and watch interdimensional cable", "Compute escape pods coordinates")
 
 if mainQuest==1:
-    print("Connecting to interdimensional cable")    
+    print("Connecting to interdimensional cable")
 
     for i in tqdm(range(0,100)):
         sleep(0.1)
@@ -223,7 +223,7 @@ elif mainQuest==2:
 
     if mainQuest == 1:
 
-        print("Connecting to interdimensional cable")    
+        print("Connecting to interdimensional cable")
 
         for i in tqdm(range(0,100)):
             sleep(0.1)
@@ -232,14 +232,13 @@ elif mainQuest==2:
     
         call("open  https://topotech.github.io/interdimensionalcable/", shell=True)
 
-        
 p=getpass("Enter pods escape coordinates [DIMENSION-RADIUS-ANGLE DDDD-RR-AAA]: ")
 
 while(p.lower() != "c137-30-135"):
     print("loading coordinates {}".format(p.lower()))
     for i in tqdm(range(0,10)):
             sleep(0.1)
-    
+
     if (p.lower() == "c137-30-045"):
         print("Not enough fuel for this escape coords")
         p=getpass("Enter pods escape coordinates: DIMENSIONS-RADIUS-ANGLE DDDD-RR-AAA: ")
